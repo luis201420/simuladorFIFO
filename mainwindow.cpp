2b2db2ec317072d2f5a6a1b8e76f8efcce937610
luis <tauro_luisito_20@hotmil.com>
2017-09-22 17:31:49 -0500

    comenzando interfaz

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,5 +1,10 @@
 #include "mainwindow.h"
 
+#include <QTextEdit>
+#include <QLabel>
+#include <QHBoxLayout>
+#include <QDebug>
+
 MainWindow::MainWindow(QRect screen)
 {
     this->height = screen.height();
@@ -12,4 +17,28 @@ MainWindow::MainWindow(QRect screen)
     this->setWindowTitle("Simulador FIFO");
     this->setStyleSheet("background-color:#C8C8C8;");
 
+    this->setMaximumSize(this->width,this->height);
+    this->setMinimumSize(this->width,this->height);
+
+    /*************************************************/
+    /**                 TEXTO                       **/
+    QLabel *label = new QLabel(this);
+    QHBoxLayout *layout = new QHBoxLayout();
+    label->setText("Numero de Procesos: ");
+    layout->addWidget(label);
+    int posTextX=(0.04)*(this->height);
+    int posTextY=(0.02)*(this->width);
+    int textWidth=120;
+    int textHeight=15;
+    label->setGeometry(posTextX,posTextY,textWidth,textHeight);
+
+    qDebug()<<textWidth;
+
+    /***************************************************/
+    /**                   TEXTEDIT                    **/
+    QTextEdit *txt = new QTextEdit();
+    txt->setParent(this);
+    txt->setGeometry((posTextX+textWidth),(posTextY-5),60,30);
+    txt->setStyleSheet("background-color:#FFFFFF;");
+    txt->show();
 }
